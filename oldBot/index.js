dac9eb32b6bba5de0a03f3c925a96345c71042c4
Guilherme Bertola <96829993+GuilhermeBertola1@users.noreply.github.com>
2023-10-21 13:06:33 -0300

    oldBot atualizado

--- oldBot/index.js
+++ oldBot/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-let paginaValue = 20;
+let paginaValue = 6;
 let lista = [];
 let listaMudaConta = [1,2,3,5,6,7,9,10,
   11,13,14,15,17,18,19,
@@ -16,80 +16,58 @@ async function webBot(){
 
   const browser = await puppeteer.launch({
     headless:false,
-    executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
   });
 
   const page = await browser.newPage();
-
-  for(var pagina = 1; pagina <= 1; pagina++){
-
-    let FatormudaConta = 0;
+  let FatormudaConta = 0;
               
-    await page.goto('https://steamcommunity.com/groups/csgocasescom/members/?p=' + paginaValue);
-    paginaValue++;
-
-    console.log(paginaValue - 1);
+  await page.goto('https://steamcommunity.com/groups/MadeInbrazil/members/?p=' + paginaValue);
+  paginaValue++;
+  console.log(paginaValue - 1);
           
-    for(var clickAccounts = 1; clickAccounts <= 51; clickAccounts++){
+  for(var clickAccounts = 1; clickAccounts <= 51; clickAccounts++){
 
-      await page.waitForTimeout(10000);
-      const conta = await page.waitForXPath('//*[@id="memberList"]/div[' + listaMudaConta[FatormudaConta] + ']/div[1]/a');
-      await conta.click();
-      await page.waitForTimeout(10000);
+    await page.waitForTimeout(5000);
+    const conta = await page.waitForXPath('//*[@id="memberList"]/div[' + listaMudaConta[FatormudaConta] + ']/div[1]/a');
+    await conta.click();
+    await page.waitForTimeout(3000);
 
-      await page.waitForSelector('.profile_header_summary');
+    await page.waitForSelector('.profile_header_summary');
 
-      const quantidadeItens = await page.evaluate(()=>{
-        const elementos = document.querySelector('.profile_header_summary').textContent;
-        return elementos;
+    const quantidadeItens = await page.evaluate(()=>{
+      const elementos = document.querySelector('.profile_header_summary').textContent;
+      return elementos;
+    });
+
+    if(quantidadeItens.length >= 196){
+      await page.waitForSelector('.friendPlayerLevelNum');
+      const level = await page.evaluate(()=>{
+        const contalevel = document.querySelector('.friendPlayerLevelNum').textContent;
+        return contalevel;
       });
+      if(level == 0){
 
-      if(quantidadeItens.length >= 196){
-  
-        await page.waitForSelector('.friendPlayerLevelNum');
-        const level = await page.evaluate(()=>{
-          const contalevel = document.querySelector('.friendPlayerLevelNum').textContent;
-          return contalevel;
+        await page.waitForSelector('.profile_header_badgeinfo_badge_area');
+        const links = await page.evaluate(()=>{
+        const link22 = document.querySelector('.profile_header_badgeinfo_badge_area').baseURI;
+          return link22;
         });
-  
-        if(level == 0){
+        lista.push(links);
 
-          await page.waitForSelector('.profile_header_badgeinfo_badge_area');
-          const links = await page.evaluate(()=>{
-            const link22 = document.querySelector('.profile_header_badgeinfo_badge_area').baseURI;
-            return link22;
-          });
-
-          lista.push(links);
-
-          fs.writeFile('listaDeContas.json', JSON.stringify(lista, null, 2), err =>{
-            if(err) throw new Error('something went wrong');
-            console.log('sucessfuly');
-          });
-
-          await page.goBack();
-
-        }else{
-                
-          await page.goBack();
-                
-        }
-
-      }else if(quantidadeItens.length <= 95){
-        
+        fs.writeFile('listaDeContas.json', JSON.stringify(lista, null, 2), err =>{
+          if(err) throw new Error('something went wrong');
+          console.log('sucessfuly');
+        });
         await page.goBack();
-
+      }else{
+        await page.goBack();  
       }
-
-      FatormudaConta++;
-            
+    }else if(quantidadeItens.length <= 95){
+      await page.goBack();
     }
-
+    FatormudaConta++;     
   }
-
   await browser.close();
-
+  webBot();
 };
-
 webBot();
-setInterval(webBot, 1200000);
